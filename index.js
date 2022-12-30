65979a98cb1a203508c57a8269a4a0dc55ecb4b0
Sri2103 <damasriharsha@gmail.com>
2022-12-29 21:44:07 -0500

    added sentry

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const orderRoutes = require('./routes/order');
 const cartRoutes = require('./routes/cart');
 const stripeRoute = require('./routes/stripe');
 const cors = require('cors');
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+
 
 
 mongoose.set('strictQuery', false)
@@ -18,6 +21,20 @@ mongoose.connect(mongo_url).then(() => console.log("connected to MongoDB"))
 .catch((err) => console.log(err))
 
 const app = express();
+Sentry.init({
+    dsn: "https://ca41199617394abea3b9ddec958d8a33@o4504366759673856.ingest.sentry.io/4504415615057920",
+    environment:process.env.NODE_ENV,
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app }),
+    ],
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"))
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("../models/User");
+const User = require("../models/user");
 const CryptoJS = require('crypto-js');
 const { response } = require("express");
 const jwt = require('jsonwebtoken');
