357aa79214969ae55af8f2debe6c142205924042
Sri2103 <damasriharsha@gmail.com>
2022-12-29 21:20:19 -0500

    fix moduling

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,6 @@
 const {verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 const CryptoJS = require('crypto-js');
-const Product = require("../models/product");
+const Product = require("../models/Product");
 const router = require("express").Router();
 
 
